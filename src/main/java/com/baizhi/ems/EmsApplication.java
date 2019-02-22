c9d8653b35541704562c8b42bdfbdc6dc83de740
hechunzhao <76036140@qq.com>
2019-02-22 11:50:33 +0800

    v2.0

--- src/main/java/com/baizhi/ems/EmsApplication.java
+++ src/main/java/com/baizhi/ems/EmsApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @MapperScan("com.baizhi.ems.dao")
 public class EmsApplication {
     public static void main(String[] args) {
+        System.out.println("暗杀哈哈哈哈");
         SpringApplication.run(EmsApplication.class, args);
     }
 
